99331caedfef6ae8c5e7c2c168792df998ba2358
MonicaIzzo <info@monicaizzo.net>
2023-05-26 14:29:11 +0200

    add steps and comments al file JS

--- js/script.js
+++ js/script.js
@@ -1,1 +1,28 @@
 console.log('JS OK')
+
+/* 
+- 1 Chiedere all'utente tramite il prompt i km che vuole percorrere.
+- 2 Chiedere all'utente tramite il prompt la sua età.
+- 3 Calcolare il prezzo del biglietto al netto degli sconti.
+- 4 Stabilire se l'utente ha diritto a sconti (young o over).
+- 5 "Normalizzare" il risultato (solo 2 decimali)  
+- 6 Stampare il risultato in pagina   
+*/ 
+
+// # 1
+
+
+
+// # 2
+
+
+// # 3
+
+
+// # 4
+
+
+// # 5
+
+
+// # 6
