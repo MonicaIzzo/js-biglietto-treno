bc3b21dbba265f26da83afc22c0efffa58171e94
MonicaIzzo <info@monicaizzo.net>
2023-05-26 14:38:05 +0200

    add JS steps (0, 1, 2)

--- js/script.js
+++ js/script.js
@@ -1,6 +1,7 @@
 console.log('JS OK')
 
 /* 
+- 0 recupero l'elemento da DOM
 - 1 Chiedere all'utente tramite il prompt i km che vuole percorrere.
 - 2 Chiedere all'utente tramite il prompt la sua età.
 - 3 Calcolare il prezzo del biglietto al netto degli sconti.
@@ -9,12 +10,17 @@ console.log('JS OK')
 - 6 Stampare il risultato in pagina   
 */ 
 
-// # 1
+// # 0
+const PrinceDiscounted = document.getElementById ('prince-discounted');
 
+// # 1
+const KMtoTravel  = prompt('Quanti km vuoi percorrere?');
+console.log(KMtoTravel);
 
 
 // # 2
-
+const youAge  = prompt('Quanti anni hai?');
+console.log(youAge);
 
 // # 3
 
